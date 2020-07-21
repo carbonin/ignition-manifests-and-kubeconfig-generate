9ba704c27ca49e42698ac0d156ea01179477cb1c
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-07-21 10:08:04 +0300

    MGMT-1318 - add annotations to BMH CRs that include data from bm-inventory (IPs etc')

--- render_files.py
+++ render_files.py
@@ -2,13 +2,19 @@
 
 import argparse
 import json
+import yaml
 import logging
 import subprocess
 import sys
 import os
 import boto3
+import base64
+import re
 from botocore.exceptions import NoCredentialsError
 
+files_to_remove = ['baremetal-provisioning-config']
+
+bmh_cr_file_pattern = 'openshift-cluster-api_hosts'
 
 def get_s3_client(s3_endpoint_url):
     aws_access_key_id = os.environ.get("aws_access_key_id", "accessKey1")
@@ -33,21 +39,104 @@ def upload_to_aws(s3_client, local_file, bucket, s3_file):
         return False
 
 
-def remove_bmo_provisioning(ignition_file):
-    found = False
+def contains_files_to_remove(path):
+    for file in files_to_remove:
+        if file in path:
+            return True
+    return False
+
+
+def is_bmh_cr_file(path):
+    if bmh_cr_file_pattern in path:
+        return True
+    return False
+
+def contains_files_to_update(path):
+    print ('YEV - check path %s' % path)
+    for file in files_to_update:
+        if file in path:
+            print ('YEV - found file to update')
+            return True
+    return False
+
+
+def get_bmh_dict_from_file(file_data):
+    source_string = file_data['contents']['source']
+    base64_string = re.split("base64,", source_string)[1]
+    decoded_string = base64.b64decode(base64_string).decode()
+    return yaml.safe_load(decoded_string)
+
+
+def prepare_annotation_dict(status_dict, hosts_list, is_master):
+    inventory_host = find_inventory_host(hosts_list, is_master)
+    if inventory_host is None:
+        return None
+
+    annot_dict = dict.copy(status_dict)
+    nics = [{'name':nic['name'], 'model':"", 'mac' : nic['mac'], 'ip': nic['ipAddr'], 'speedGbps': nic['speed']} for nic in inventory_host['nics']]
+    cpu = prepare_cpu_hardware()
+    storage = prepare_storage()
+    hardware = {'nics': nics, 'cpu': cpu, 'storage': storage}
+    annot_dict['hardware'] = hardware
+    hosts_list.remove(inventory_host)
+    return {'baremetalhost.metal3.io/status': json.dumps(annot_dict)}
+
+
+def prepare_cpu_hardware():
+    flags = []
+    return {'arch': "", 'model':"", 'clockMegahertz': 0.0, 'count': 1, 'flags': flags}
+
+
+def prepare_storage():
+    return []
+
+
+def set_new_bmh_dict_in_file(file_data, bmh_dict):
+    decoded_string = yaml.dump(bmh_dict)
+    base64_string = base64.b64encode(decoded_string.encode())
+    source_string = 'data:text/plain;charset=utf-8;' + 'base64,' + base64_string.decode()
+    file_data['contents']['source'] = source_string
+
+
+def is_master_bmh(bmh_dict):
+    if "-master-" in bmh_dict['metadata']['name']:
+        return True
+    return False
+
+
+def find_inventory_host(hosts_list, is_master):
+    role_to_find = 'master' if is_master else 'worker'
+    for host in hosts_list:
+        if host['role'] == role_to_find:
+            return host
+    return None
+
+
+def update_bmh_cr_file(file_data, hosts_list):
+    bmh_dict = get_bmh_dict_from_file(file_data)    
+    annot_dict = prepare_annotation_dict(bmh_dict['status'], hosts_list, is_master_bmh(bmh_dict))
+    if annot_dict is not None:
+        bmh_dict['spec']['bmc']['credentialsName'] = ''
+        bmh_dict['metadata']['annotations'] = annot_dict
+        set_new_bmh_dict_in_file(file_data, bmh_dict)
+
+
+def remove_bmo_files(ignition_file, bmh_config_str):
+    bmh_config = yaml.safe_load(bmh_config_str)
+
+    hosts_list = bmh_config['hosts']
     with open(ignition_file, "r") as file_obj:
         data = json.load(file_obj)
         storage_files = data['storage']['files']
         # Iterate through a copy of the list
         for file_data in storage_files[:]:
-            if 'baremetal-provisioning-config' in file_data['path']:
-                storage_files.remove(file_data)
-                found = True
-                break
-    if found:
-        with open(ignition_file, "w") as file_obj:
-            json.dump(data, file_obj)
+            # if contains_files_to_remove(file_data['path']):
+            #    storage_files.remove(file_data)
+            if is_bmh_cr_file(file_data['path']):
+                update_bmh_cr_file(file_data, hosts_list)
 
+    with open(ignition_file, "w") as file_obj:
+        json.dump(data, file_obj)
 
 def upload_to_s3(s3_endpoint_url, bucket, install_dir):
     s3_client = get_s3_client(s3_endpoint_url)
@@ -97,14 +186,23 @@ def main():
     if not os.path.isfile(os.path.join(config_dir, 'install-config.yaml')):
         raise Exception("install config file not located in installer dir")
 
-    command = "OPENSHIFT_INSTALL_INVOKER=\"assisted-installer\" %s/openshift-install create ignition-configs --dir %s" % (work_dir, config_dir)
+    try:
+        # command = "%s/oc adm release extract --command=openshift-baremetal-install  --to=%s quay.io/openshift-release-dev/ocp-release-nightly@sha256:ba2e09a06c7fca19e162286055c6922135049e6b91f71e2a646738b2d7ab9983" % (work_dir, work_dir)
+        command = "%s/oc adm release extract --command=openshift-baremetal-install  --to=%s quay.io/openshift-release-dev/ocp-release-nightly@sha256:b0600325129b5b14d272ad61bcbd7fe609b812ac2620976158046a7bd2c31c62" % (work_dir, work_dir)
+        subprocess.check_output(command, shell=True, stderr=sys.stdout)
+    except Exception as ex:
+        raise Exception('Failed to extract installer, exception: {}'.format(ex))
+
+    command = "OPENSHIFT_INSTALL_INVOKER=\"assisted-installer\" %s/openshift-baremetal-install create ignition-configs --dir %s" % (work_dir, config_dir)
     try:
         subprocess.check_output(command, shell=True, stderr=sys.stdout)
     except Exception as ex:
         raise Exception('Failed to generate files, exception: {}'.format(ex))
 
     try:
-        remove_bmo_provisioning("%s/bootstrap.ign" % config_dir)
+        bmh_config = os.environ.get("BMH_CONFIG")
+        # bmh_config = "hosts:\n- role: master\n  hostname: test-bmh1-master-0.redhat.com\n  nics:\n  - name: eth0\n    mac: 52:54:00:4e:ea:7b\n    ipAddr: 192.168.126.10\n    speed: -1\n  - name: eth1\n    mac: 52:54:00:4e:19:64\n    ipAddr: 192.168.140.220\n    speed: -1\n- role: master\n  hostname: test-bmh1-master-1.redhat.com\n  nics:\n  - name: eth0\n    mac: 52:54:00:c2:de:42\n    ipAddr: 192.168.126.11\n    speed: -1\n  - name: eth1\n    mac: 52:54:00:3e:3a:53\n    ipAddr: 192.168.140.120\n    speed: -1\n- role: master\n  hostname: test-bmh1-master-2.redhat.com\n  nics:\n  - name: eth0\n    mac: 52:54:00:2e:c4:dd\n    ipAddr: 192.168.126.12\n    speed: -1\n  - name: eth1\n    mac: 52:54:00:0e:4e:20\n    ipAddr: 192.168.140.204\n    speed: -1\n"
+        remove_bmo_files("%s/bootstrap.ign" % config_dir, bmh_config)
     except Exception as ex:
         raise Exception('Failed to remove BMO prosioning configuration from bootstrap ignition, exception: {}'.format(ex))
 
