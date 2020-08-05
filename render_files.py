0895e06946f63099b86948d7aa52c5b565f235f4
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-08-05 17:22:08 +0300

    MGMT-1620 - preparation for 4.6: extract openshift-baremetal-installer from OCP release and pull secret from install-config

--- render_files.py
+++ render_files.py
@@ -140,8 +140,8 @@ def create_config_dir(work_dir):
 
 def extract_baremetal_installer(work_dir, openshift_release_image):
     try:
-        command = "%s/oc adm release extract --command=openshift-baremetal-install  --to=%s %s" \
-                % (work_dir, work_dir, openshift_release_image)
+        command = "{oc_dir}/oc adm release extract --command=openshift-baremetal-install  --to={out_dir} {release_image}".format(
+            oc_dir=work_dir, out_dir=work_dir, release_image=openshift_release_image)
         subprocess.check_output(command, shell=True, stderr=sys.stdout)
     except Exception as ex:
         raise Exception('Failed to extract installer, exception: {}'.format(ex))
